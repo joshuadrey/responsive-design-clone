cc7dfe57bf7785720cb45d19f40fc92ef83efcb8
Joshua Leavitt <joshuadreyleavitt@gmail.com>
2021-05-06 11:41:37 -0600

    got most things to display

---
+++ start-bootstrap/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pageone from './components/Pageone'
+
+function App() {
+  return (
+    <div className="App">
+      <Pageone />
+    </div>
+  );
+}
+
+export default App;
